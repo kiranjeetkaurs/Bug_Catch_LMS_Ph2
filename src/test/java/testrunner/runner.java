cc3268a3cd518d97a66636e513ff9565f6c3f645
kiranjeetkaurs <kiranjeetkaurs@gmail.com>
2022-09-26 22:42:46 -0500

    test

--- src/test/java/testrunner/runner.java
+++ src/test/java/testrunner/runner.java
@@ -1,13 +1,15 @@
 package testrunner;
 
 
+import org.testng.annotations.Test;
+
 //import org.junit.runner.RunWith;
 
 //import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
+@Test
 //@RunWith(Cucumber.class)
 @CucumberOptions(
 	  plugin = {"pretty"},
