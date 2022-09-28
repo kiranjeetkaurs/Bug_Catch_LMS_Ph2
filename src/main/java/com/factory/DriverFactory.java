785b01c56af761f285a6c7a77bb3efc5cb5e4e4a
kiranjeetkaurs <kiranjeetkaurs@gmail.com>
2022-09-28 17:10:44 -0500

    Update DriverFactory.java

--- src/main/java/com/factory/DriverFactory.java
+++ src/main/java/com/factory/DriverFactory.java
@@ -43,7 +43,7 @@ public class DriverFactory {
 		return getDriver();
 	}
 	
-	//!!!!!!!!! !!!!!!!!!!!@@@@@@@@@@@This is used to get the driver with threadlocal
+	//!!!!!wwwwwwww!!!! !!!!!!!!!!!@@@@@@@@@@@This is used to get the driver with threadlocal
 	public static synchronized WebDriver getDriver() {
 		return tlDriver.get();
 
